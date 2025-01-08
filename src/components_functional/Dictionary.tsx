bf053247800b57e02f7febc76d029421567c7e5e
igrisdev <johanalvarezz237@gmail.com>
2025-01-08 15:04:44 -0500

    Add Dictionary component to display WordReference translation

---
+++ src/components_functional/Dictionary.tsx
@@ -0,0 +1,10 @@
+export const Dictionary = () => {
+  return (
+    <div className='w-full h-full'>
+      <iframe
+        src='https://www.wordreference.com/es/translation.asp?tranword=happy'
+        className='w-full h-full'
+      ></iframe>
+    </div>
+  )
+}
