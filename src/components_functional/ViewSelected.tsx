e694a6dc4e72199a50f63e74fe491ecc3b7863b8
igrisdev <johanalvarezz237@gmail.com>
2025-01-10 11:28:21 -0500

    Refactor ViewSelected component to move VIEWS constant outside of the component function

--- src/components_functional/ViewSelected.tsx
+++ src/components_functional/ViewSelected.tsx
@@ -4,14 +4,14 @@ import { Dictionary } from '@components_functional/Dictionary'
 import { ViewImages } from '@components_functional/ViewImages'
 import { useEffect } from 'react'
 
+const VIEWS = {
+  DICTIONARY: <Dictionary />,
+  IMAGES: <ViewImages />,
+  SENTENCES: <div>Sentences</div>,
+}
+
 export const ViewSelected = () => {
   const { view } = useConfigStore()
-
-  const VIEWS = {
-    DICTIONARY: <Dictionary />,
-    IMAGES: <ViewImages />,
-    SENTENCES: <div>Sentences</div>,
-  }
 
   const Selected = () => {
     if (view) {
