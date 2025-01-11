842ccf44ba9c8db504915ebe7e0da2cee2a5c3cb
igrisdev <johanalvarezz237@gmail.com>
2025-01-10 21:02:59 -0500

    Enhance ContainerText component layout for improved click event handling and readability

--- src/components_functional/ContainerText.tsx
+++ src/components_functional/ContainerText.tsx
@@ -15,8 +15,56 @@ export const ContainerText = () => {
   }
 
   return (
-    <div onClick={handleClick} className='bg-base-300 p-2 rounded-sm'>
-      <p className='flex flex-wrap gap-1'>
+    <div
+      onClick={handleClick}
+      className='bg-base-300 p-2 rounded-sm [&>p]:flex [&>p]:flex-wrap [&>p]:gap-x-1 flex flex-col gap-y-2 [&>p>span]:cursor-pointer'
+    >
+      <p>
+        <span>Hello</span>
+        <span>World</span>
+        <span>This</span>
+        <span>is</span>
+        <span>a</span>
+        <span>test</span>
+        <span>text</span>
+        <span>to</span>
+        <span>see</span>
+        <span>if</span>
+        <span>the</span>
+        <span>click</span>
+        <span>event</span>
+        <span>works</span>
+        <span>Hello</span>
+        <span>World</span>
+        <span>This</span>
+        <span>is</span>
+        <span>a</span>
+        <span>test</span>
+        <span>text</span>
+        <span>to</span>
+        <span>see</span>
+        <span>if</span>
+        <span>the</span>
+        <span>click</span>
+        <span>event</span>
+        <span>works</span>
+        <span>Hello</span>
+        <span>World</span>
+        <span>This</span>
+        <span>is</span>
+        <span>a</span>
+        <span>test</span>
+        <span>text</span>
+        <span>to</span>
+        <span>see</span>
+        <span>if</span>
+        <span>the</span>
+        <span>click</span>
+        <span>event</span>
+        <span>works</span>
+      </p>
+
+      <p>
         <span>Hello</span>
         <span>World</span>
         <span>This</span>
