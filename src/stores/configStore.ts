2af787f4a784fe2e690c9369a25f8eeb366e05b8
igrisdev <johanalvarezz237@gmail.com>
2025-01-13 19:22:38 -0500

    Add language property to config store with default value

--- src/stores/configStore.ts
+++ src/stores/configStore.ts
@@ -5,10 +5,12 @@ type View = 'DICTIONARY' | 'IMAGES' | 'SENTENCES'
 
 type ConfigStore = {
   view: View,
+  language: string,
   setView: (view: string) => void
 }
 
 export const useConfigStore = create<ConfigStore>()((set) => ({
   view: 'DICTIONARY',
+  language: 'english',
   setView: (view: string) => set({ view: view as View }),
 }))
