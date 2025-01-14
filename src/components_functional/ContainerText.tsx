65f9c62fecde46b9cf618205398e1ee7f26b8134
igrisdev <johanalvarezz237@gmail.com>
2025-01-13 19:47:53 -0500

    Refactor ContainerText component to render sentences dynamically from an array

--- src/components_functional/ContainerText.tsx
+++ src/components_functional/ContainerText.tsx
@@ -2,6 +2,12 @@ import { useConfigStore } from '@stores/configStore'
 import { useLanguagesStore } from '@stores/languagesStore'
 import { useSearchStore } from '@stores/searchStore'
 
+const sentences = [
+  ['Hello', 'World', 'This', 'is', 'a', 'test', 'text'],
+  ['to', 'see', 'if', 'the', 'click', 'event', 'works'],
+  ['Hello', 'World', 'This', 'is', 'a', 'test', 'text'],
+]
+
 export const ContainerText = () => {
   const { searchWord } = useSearchStore()
   const { view, setView } = useConfigStore()
@@ -14,8 +20,6 @@ export const ContainerText = () => {
 
       if (view !== 'DICTIONARY') setView('DICTIONARY')
     }
-
-    console.log(languages)
   }
 
   return (
@@ -23,95 +27,13 @@ export const ContainerText = () => {
       onClick={handleClick}
       className='bg-base-300 p-2 rounded-sm [&>p]:flex [&>p]:flex-wrap [&>p]:gap-x-1 flex flex-col gap-y-2 [&>p>span]:cursor-pointer'
     >
-      <p>
-        <span>Hello</span>
-        <span>World</span>
-        <span>This</span>
-        <span>is</span>
-        <span>a</span>
-        <span>test</span>
-        <span>text</span>
-        <span>to</span>
-        <span>see</span>
-        <span>if</span>
-        <span>the</span>
-        <span>click</span>
-        <span>event</span>
-        <span>works</span>
-        <span>Hello</span>
-        <span>World</span>
-        <span>This</span>
-        <span>is</span>
-        <span>a</span>
-        <span>test</span>
-        <span>text</span>
-        <span>to</span>
-        <span>see</span>
-        <span>if</span>
-        <span>the</span>
-        <span>click</span>
-        <span>event</span>
-        <span>works</span>
-        <span>Hello</span>
-        <span>World</span>
-        <span>This</span>
-        <span>is</span>
-        <span>a</span>
-        <span>test</span>
-        <span>text</span>
-        <span>to</span>
-        <span>see</span>
-        <span>if</span>
-        <span>the</span>
-        <span>click</span>
-        <span>event</span>
-        <span>works</span>
-      </p>
-
-      <p>
-        <span>Hello</span>
-        <span>World</span>
-        <span>This</span>
-        <span>is</span>
-        <span>a</span>
-        <span>test</span>
-        <span>text</span>
-        <span>to</span>
-        <span>see</span>
-        <span>if</span>
-        <span>the</span>
-        <span>click</span>
-        <span>event</span>
-        <span>works</span>
-        <span>Hello</span>
-        <span>World</span>
-        <span>This</span>
-        <span>is</span>
-        <span>a</span>
-        <span>test</span>
-        <span>text</span>
-        <span>to</span>
-        <span>see</span>
-        <span>if</span>
-        <span>the</span>
-        <span>click</span>
-        <span>event</span>
-        <span>works</span>
-        <span>Hello</span>
-        <span>World</span>
-        <span>This</span>
-        <span>is</span>
-        <span>a</span>
-        <span>test</span>
-        <span>text</span>
-        <span>to</span>
-        <span>see</span>
-        <span>if</span>
-        <span>the</span>
-        <span>click</span>
-        <span>event</span>
-        <span>works</span>
-      </p>
+      {sentences.map((sentence, index) => (
+        <p key={index}>
+          {sentence.map((word, index) => (
+            <span key={index}>{word}</span>
+          ))}
+        </p>
+      ))}
     </div>
   )
 }
