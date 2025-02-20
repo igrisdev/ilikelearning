8283df0cc3fa734b6d3dc77fb18561c5552a959b
igrisdev <johanalvarezz237@gmail.com>
2025-02-19 20:01:00 -0500

    Refactor searchStore to improve code formatting and consistency

--- src/stores/searchStore.ts
+++ src/stores/searchStore.ts
@@ -1,13 +1,13 @@
 import { create } from 'zustand'
 
 type SearchStore = {
-  word: string,
-  dictionaryUrl: string,
-  imagesUrl: string,
+  word: string
+  dictionaryUrl: string
+  imagesUrl: string
   searchWord: (word: string) => void
 }
 
-export const useSearchStore = create<SearchStore>()((set) => ({
+export const useSearchStore = create<SearchStore>()(set => ({
   word: '',
   dictionaryUrl: 'https://www.wordreference.com/es/translation.asp?tranword=',
   imagesUrl: 'https://api.pexels.com/v1/search?query=',
