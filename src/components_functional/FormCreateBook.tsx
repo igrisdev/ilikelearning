d968cb242476c95fcee754b9a72e6ae3b09dedf6
igrisdev <johanalvarezz237@gmail.com>
2025-02-27 23:15:09 -0500

    Refactor FormCreateBook component to manage book creation and validation; update form fields and local storage handling

--- src/components_functional/FormCreateBook.tsx
+++ src/components_functional/FormCreateBook.tsx
@@ -1,45 +1,77 @@
-import { useState, type FormEvent, type ChangeEvent } from 'react'
+import { useState, type FormEvent, type ChangeEvent, useEffect } from 'react'
+
+interface iBook {
+  title: string
+  description: string
+  language: string
+  tags: string[]
+  // file: File | null
+}
 
 export const FormCreateBook = () => {
-  const [fileContent, setFileContent] = useState<string | null>(null)
-  const [pdfURL, setPdfURL] = useState<string | null>(null)
+  const [books, setBooks] = useState<iBook[]>([])
 
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0]
+  useEffect(() => {
+    setBooks(initialBookStore())
+  }, [])
 
-    if (file) {
-      const fileType = file.type
-
-      if (fileType === 'text/plain') {
-        // Leer archivos .txt
-        const reader = new FileReader()
-        reader.onload = event => {
-          setFileContent(event.target?.result as string)
-          setPdfURL(null) // Limpiar vista previa PDF
-        }
-        reader.readAsText(file)
-      } else if (fileType === 'application/pdf') {
-        // Mostrar PDF en un iframe
-        const url = URL.createObjectURL(file)
-        setPdfURL(url)
-        setFileContent(null) // Limpiar texto previo
-      } else {
-        setFileContent(
-          'No se puede mostrar el contenido de este tipo de archivo.'
-        )
-        setPdfURL(null)
-      }
+  function initialBookStore(): iBook[] {
+    const isBooks = JSON.parse(localStorage.getItem('books') as string) as {
+      listBooks: iBook[]
     }
+
+    if (isBooks) {
+      return isBooks.listBooks
+    }
+
+    localStorage.setItem('books', JSON.stringify({ listBooks: [] }))
+    return []
+  }
+
+  const createBook = (book: iBook) => {
+    if (!books) return
+
+    const newBooks: iBook[] | [] = [...books, book]
+
+    localStorage.setItem('books', JSON.stringify({ listBooks: newBooks }))
+    setBooks(newBooks)
+  }
+
+  const validateBook = (book: any) => {
+    if (!book.title) {
+      return 'El título es obligatorio'
+    }
+
+    if (!book.description) {
+      return 'La descripción es obligatoria'
+    }
+
+    if (!book.language) {
+      return 'El idioma es obligatorio'
+    }
+
+    if (!book.tags.length) {
+      return 'Las etiquetas son obligatorias'
+    }
+
+    return null
   }
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     const formData = new FormData(e.currentTarget)
-    // console.log(
-    //   'Datos del formulario:',
-    //   formData.get('title'),
-    //   formData.get('fileUpload')
-    // )
+
+    validateBook(formData)
+
+    const book = {
+      title: formData.get('title') as string,
+      description: formData.get('description') as string,
+      language: formData.get('language') as string,
+      tags: (formData.get('tags') as string).split(',').map(tag => tag.trim()),
+      // file: formData.get('fileUpload'),
+    }
+
+    createBook(book)
   }
 
   return (
@@ -55,35 +87,31 @@ export const FormCreateBook = () => {
       </div>
 
       <div>
-        <label htmlFor='fileUpload'>Archivo (.txt, .pdf, .epub)</label>
+        <label htmlFor='description'>Descripción</label>
+        <textarea
+          name='description'
+          id='description'
+          className='border p-2 rounded w-full'
+        ></textarea>
+      </div>
+
+      <div>
+        <label htmlFor='language'>Idioma</label>
+        <select name='language' id='language' className='border p-2 rounded'>
+          <option value=' '>Seleccionar</option>
+          <option value='English'>English</option>
+        </select>
+      </div>
+
+      <div>
+        <label htmlFor='tags'>Tags</label>
         <input
-          type='file'
-          id='fileUpload'
-          name='fileUpload'
-          accept='.txt, .pdf, .epub'
-          onChange={handleFileChange}
+          type='text'
+          name='tags'
+          id='tags'
           className='border p-2 rounded w-full'
         />
       </div>
-
-      {fileContent && (
-        <div className='p-2 border rounded bg-gray-100'>
-          <h3 className='font-bold'>Contenido del archivo:</h3>
-          <pre className='whitespace-pre-wrap text-sm'>{fileContent}</pre>
-        </div>
-      )}
-
-      {pdfURL && (
-        <div className='mt-4'>
-          <h3 className='font-bold mb-2'>Vista previa del PDF:</h3>
-          <iframe
-            src={pdfURL}
-            width='100%'
-            height='400px'
-            className='border rounded'
-          ></iframe>
-        </div>
-      )}
 
       <button type='submit' className='bg-blue-500 text-white p-2 rounded'>
         Crear Libro
