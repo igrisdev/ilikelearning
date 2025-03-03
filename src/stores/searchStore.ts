d6fcabf231254424d2d50a9d270036538b5af08d
igrisdev <johanalvarezz237@gmail.com>
2025-03-03 18:19:44 -0500

    Refactor search store to include text translation functionality and update dictionary URL generation

--- src/stores/searchStore.ts
+++ src/stores/searchStore.ts
@@ -2,15 +2,23 @@ import { create } from 'zustand'
 
 type SearchStore = {
   words: string
-  dictionaryUrl: string
+  textTranslated: string
   imagesUrl: string
+  setTextTranslated: (text: string) => void
   searchWord: (words: string) => void
+  dictionaryUrl: (words: string) => string
 }
 
 export const useSearchStore = create<SearchStore>()(set => ({
   words: '',
-  dictionaryUrl:
-    'https://www.deepl.com/es/translator/q/es/diccionario/en/dictionary/1002a452e',
+  textTranslated: '',
   imagesUrl: 'https://api.pexels.com/v1/search?query=',
+  setTextTranslated: (text: string) => set({ textTranslated: text }),
   searchWord: (words: string) => set({ words }),
+  dictionaryUrl: words => {
+    let text = encodeURIComponent(words)
+    const url = `https://api.mymemory.translated.net/get?q=${text}&langpair=en|es-CO`
+
+    return url
+  },
 }))
