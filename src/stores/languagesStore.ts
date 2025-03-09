afc2d315f956be6e95b4f3e01c8aa117a2f7fd5b
igrisdev <johanalvarezz237@gmail.com>
2025-03-08 21:17:20 -0500

    Refactor languagesStore to use interfaces and improve addWord functionality

--- src/stores/languagesStore.ts
+++ src/stores/languagesStore.ts
@@ -1,29 +1,46 @@
 import { create } from 'zustand'
 
-type Languages = {
-  [key: string]: Word[]
-}
-
-type Word = {
+export interface iWord {
   words: string
   translation: string
   level: number
   group: string
-  // image: string
 }
 
-type LanguagesStore = {
-  languages: Languages
-  addWord: (words: Word, language: string) => void
+interface iLanguage {
+  name: string
+  words: iWord[]
 }
 
-export const useLanguagesStore = create<LanguagesStore>()(set => ({
-  languages: {},
-  addWord: (words: Word, language: string) =>
-    set(({ languages }) => ({
-      languages: {
-        ...languages,
-        [language]: [...(languages[language] || []), words],
-      },
-    })),
+type iLanguagesStore = {
+  languages: iLanguage[] | []
+  addWordInLanguage: (word: iWord, language: string) => void
+}
+
+export const useLanguagesStore = create<iLanguagesStore>()(set => ({
+  languages: [],
+
+  addWordInLanguage: (word: iWord, language: string) => {
+    set(({ languages }) => {
+      const updatedLanguages = [...languages]
+
+      const languageIndex = updatedLanguages.findIndex(
+        lang => lang.name === language
+      )
+
+      if (languageIndex !== -1) {
+        updatedLanguages[languageIndex] = {
+          ...updatedLanguages[languageIndex],
+          words: [...updatedLanguages[languageIndex].words, word],
+        }
+      } else {
+        updatedLanguages.push({
+          name: language,
+          words: [word],
+        })
+      }
+
+      return { languages: updatedLanguages }
+    })
+  },
 }))
